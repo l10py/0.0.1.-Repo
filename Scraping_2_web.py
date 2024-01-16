28645e57f1291a0730f93ab373e6e5d469a907bd
l10py <meidinliopy@gmail.com>
2024-01-16 17:39:00 +0700

    Update Scraping_2_web.py

---
+++ Scraping_2_web.py
@@ -0,0 +1,9 @@
+import requests
+import bs4 import beautufSoup
+from selenium import webdriver
+
+
+list_link=["https://www.blibli.com/p/hi-perf-4t-900-10w-50-oli-motor-fully-synthetic-1l/ps--TOE-70213-00014",
+"https://www.blibli.com/p/hi-perf-4t-500-scooter-10w-30-oli-motor-matic-0-8-l/ps--TOE-70213-00055"]
+
+
