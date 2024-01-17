3c254a8975850c52c7ce98ba5dd2733059b9f1c7
l10py <meidinliopy@gmail.com>
2024-01-18 00:45:59 +0700

    Update Scraping_2_web.py

--- Scraping_2_web.py
+++ Scraping_2_web.py
@@ -1,9 +1,13 @@
 import requests
-import bs4 import beautufSoup
+from bs4 import BeautifulSoup
 from selenium import webdriver
 
+list_link = ["https://www.blibli.com/p/hi-perf-4t-900-10w-50-oli-motor-fully-synthetic-1l/ps--TOE-70213-00014",
+             "https://www.blibli.com/p/hi-perf-4t-500-scooter-10w-30-oli-motor-matic-0-8-l/ps--TOE-70213-00055"]
 
-list_link=["https://www.blibli.com/p/hi-perf-4t-900-10w-50-oli-motor-fully-synthetic-1l/ps--TOE-70213-00014",
-"https://www.blibli.com/p/hi-perf-4t-500-scooter-10w-30-oli-motor-matic-0-8-l/ps--TOE-70213-00055"]
-
-
+for i in list_link:
+    print(f"data={i}")
+    browser = webdriver.Chrome()  # Create a new browser instance for each link
+    browser.get(i)  # Open the specific link
+    # Perform any actions you need on the opened page
+    browser.quit()  # Close the browser after finishing with the current link
